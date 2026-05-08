be9919f3662931b40dc4549fcf8c4aa93254b38f
Vindhya H V <vindhyathod@gmail.com>
2026-05-08 14:40:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,33 @@
 function scrollToTop() {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
 }
+
+
+// Card animation
 document.querySelectorAll('.card').forEach(card => {
-    card.addEventListener('mouseenter', () => card.style.transform = 'scale(1.05)');
-    card.addEventListener('mouseleave', () => card.style.transform = 'scale(1)');
+
+    card.addEventListener('mouseenter', () => {
+        card.style.transform = 'scale(1.05)';
+    });
+
+    card.addEventListener('mouseleave', () => {
+        card.style.transform = 'scale(1)';
+    });
+
 });
+
+
+// Fetch statistics from backend
+async function loadStats() {
+
+    const response = await fetch("https://t7-the-addiction-recovery-gap-1.onrender.com");
+
+    const data = await response.json();
+
+    console.log(data);
+}
+
+loadStats();
